affe7205b17ca6b3b47a6623e153b9bcb5607aa0
Veronica Nixon <vsnixon@gmail.com>
2020-09-23 20:14:49 -0700

    challenge #1 basic code ready but print statements not formatted yet

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,19 +13,44 @@ import os
 
 month_list = []
 month_count = 0
+net_profit = 0
 
 #Read store relative path of csv for portability.
 curr_dir = os.getcwd()
 csvpath = os.path.join(curr_dir, "Resources")
 os.chdir(csvpath)
 
-with open("budget_data.csv") as datafile:
-    next(datafile) #Skip header row
-    data_rows = csv.reader(datafile, delimiter = ',')
+with open("budget_data.csv") as data_file:
+    next(data_file) #Skip header row
+    data_rows = csv.reader(data_file, delimiter = ',')
+
+    greatest_profit = 0
+    greatest_loss = 0
+
     for row in data_rows:
-        #Accummulate list of unique months
+        #Count months by accummulating them into a list (checks for duplicates)
         if row[0] not in month_list:
             month_list.append(row[0])
             month_count += 1
+        #Accummulate net profit
+        net_profit = net_profit + int(row[1])
+        if int(row[1]) > int(greatest_profit):
+            greatest_profit = row[1]
+            greatest_profit_info = [row[0], row[1]]
+        elif int(row[1]) < int(greatest_loss):
+            greatest_loss = row[1]
+            greatest_loss_info = [row[0], row[1]]
+
+
 #Print number of months in dataset
-print(str(month_count))
+print(f"Total months: {str(month_count)}")
+#Print net profit
+print(f"Net profit: {str(net_profit)}")
+#Print average change
+print(f"Average change: {str(net_profit / month_count)}")
+#Print greatest increase in profit
+print(f"Greatest increase in profit: {greatest_profit_info[1]} on {greatest_profit_info[0]}")
+#Print greatest decrease in profit
+print(f"Greatest decrease inprofit: {greatest_loss_info[1]} on {greatest_loss_info[0]}")
+
+
