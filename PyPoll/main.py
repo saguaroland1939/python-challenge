0564a031f96ed9b4a1762c0914eedcf5b81a0bf4
Veronica Nixon <vsnixon@gmail.com>
2020-09-26 21:22:05 -0700

    writes partial results to file, need to amass formatted string and print simultaneously to file and terminal

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -21,8 +21,7 @@ winner_name = ""
 
 #Open and read file, printing header row
 with open(csvpath, "r") as data_file:
-    header_row = next(data_file)
-    print(header_row)
+    next(data_file)
     data_rows = csv.reader(data_file, delimiter = ',')
         
     #Loop through dataset to count total number of votes cast and print list of candidates
@@ -37,25 +36,33 @@ with open(csvpath, "r") as data_file:
     print(f"Total votes cast: {total_vote_count}")
     print("****************************************")
 
-    #Compare each candidate in data file with each candidate in list of candidates to tally each candidate's votesCompute percentage of votes won by each candidate and compute the winner
+    
     data_rows = reset_csv(data_file)
+    #Print votes and percentage of votes for each candidate, then compute winner.
     for candidate in candidate_list:
         for row in data_rows:
             if row[2] == candidate:
                 candidate_vote_count +=1
         percent_votes = round(candidate_vote_count / total_vote_count * 100, 3)
+        #Print results to terminal (Part II)
         print(f"{candidate:9} {percent_votes}% ({candidate_vote_count})")
-        
         if candidate_vote_count > highest_vote_count:
             highest_vote_count = candidate_vote_count
             winner_name = candidate
         candidate_vote_count = 0
         data_rows = reset_csv(data_file)
-#Print election results to a csv file
-#setwd("Analysis")
-#with open('election_results.csv', 'w', newline='') as file:
 
-#Print election results to terminal (Part II)
+#Print results to terminal (Part III)
 print("****************************************")
 print(f"Winner: {winner_name}")
 print("****************************************")
+
+#Write results to csv file in Analysis directory
+output_directory = os.path.join(curr_dir, "Analysis")
+os.chdir(output_directory)
+with open('election_results.csv', 'w', newline='') as output_file:
+    output_file.write("Election results\r")
+    output_file.write("********************\r")
+    output_file.write(f"Total votes cast: {total_vote_count}\r")
+    output_file.write("********************\r")
+    output_file.write(winner_name)
