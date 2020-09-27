fea65eeee7ee34cfe6e197ff63e107494aadfd0d
Veronica Nixon <vsnixon@gmail.com>
2020-09-26 19:01:25 -0700

    vote tallies are working!

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,14 @@
 import csv
 import os
 
+#Resets csv reader to start of file
+def reset_csv(data_file):
+    data_file.seek(0)
+    header_row = next(data_file)
+    subset = [next(data_file) for x in range(300)]
+    data_rows = csv.reader(subset, delimiter = ',')
+    return data_rows
+
 #Store relative path to csv
 curr_dir = os.getcwd()
 csvpath = os.path.join(curr_dir, "Resources", "election_data.csv")
@@ -15,24 +23,30 @@ winner_name = ""
 with open(csvpath, "r") as data_file:
     header_row = next(data_file) #Skip header row
     print(header_row)
-    subset = [next(data_file) for x in range(1000)]
+    subset = [next(data_file) for x in range(300)]
     data_rows = csv.reader(subset, delimiter = ',')
-    #Create list of candidates
+        
+    #Loop through dataset to count total number of votes and print list of candidates
     for row in data_rows:
         total_vote_count += 1
         if row[2] not in candidate_list:
             candidate_list.append(row[2])
-    #Compute percentage of votes won by each candidate and computer winner
+    print(candidate_list)
+    data_rows = reset_csv(data_file)
+
+    #Compute percentage of votes won by each candidate and compute the winner
     for candidate in candidate_list:
         for row in data_rows:
             if row[2] == candidate:
                 candidate_vote_count +=1
-        print(candidate_vote_count)
+    
+        print(f"{candidate}  {candidate_vote_count}") #Prints the candidate fine, but not their votes
         percent_votes = candidate_vote_count / total_vote_count
         if candidate_vote_count > highest_vote_count:
             highest_vote_count = candidate_vote_count
             winner_name = candidate
-
+        candidate_vote_count = 0
+        data_rows = reset_csv(data_file)
 #Print election results to a csv file
 #setwd("Analysis")
 #with open('election_results.csv', 'w', newline='') as file:
