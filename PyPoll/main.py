48ad57f2830171655e8eaf6f702d278c7dd09ecc
Veronica Nixon <vsnixon@gmail.com>
2020-09-27 17:30:21 -0700

    Pypoll complete, need to fix Pybank output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,12 @@
+#This program takes a csv file of election results as input
+#and returns a summary report. The input file should have a header
+#row and include voter ID and candidate name. The program loops 
+#through the file to count the number of votes and enumerate 
+#the candidates. Next, the program loops through each of the 
+#candidates and tallies the number of votes each received to 
+#determine the winner. A report is printed to a csv file called 
+#election_results, located in the Analysis folder.
+
 import csv
 import os
 
@@ -9,8 +18,8 @@ def reset_csv(data_file):
     return data_rows
 
 #Store relative path to csv
-curr_dir = os.getcwd()
-csvpath = os.path.join(curr_dir, "Resources", "election_data.csv")
+#curr_dir = os.getcwd()
+csvpath = os.path.join("Resources", "election_data.csv")
 
 #Initialize variables
 total_vote_count = 0
@@ -18,51 +27,47 @@ candidate_list = []
 candidate_vote_count = 0
 highest_vote_count = 0
 winner_name = ""
+report = ""
 
 #Open and read file, printing header row
 with open(csvpath, "r") as data_file:
     next(data_file)
     data_rows = csv.reader(data_file, delimiter = ',')
         
-    #Loop through dataset to count total number of votes cast and print list of candidates
+    #Loop through dataset to count total number of votes cast 
+    #and print list of candidates
     for row in data_rows:
         total_vote_count += 1
         if row[2] not in candidate_list:
             candidate_list.append(row[2])
-
-    #Print election results to terminal (Part I)
-    print("Election results")
-    print("****************************************")
-    print(f"Total votes cast: {total_vote_count}")
-    print("****************************************")
-
     
+    #Save election results to formatted report for later output (Part I)
+    report = f"\n    Election results \n*************************"\
+    f"\nTotal votes cast: {total_vote_count} \n*************************"
+
     data_rows = reset_csv(data_file)
-    #Print votes and percentage of votes for each candidate, then compute winner.
+    #Print votes and percentage of votes for each candidate,
+    #then compute winner.
     for candidate in candidate_list:
         for row in data_rows:
             if row[2] == candidate:
                 candidate_vote_count +=1
         percent_votes = round(candidate_vote_count / total_vote_count * 100, 3)
-        #Print results to terminal (Part II)
-        print(f"{candidate:9} {percent_votes}% ({candidate_vote_count})")
+        #Append election results to report for later output (Part II)
+        report = report + f"\n{candidate:9} {percent_votes}% {candidate_vote_count}"
         if candidate_vote_count > highest_vote_count:
             highest_vote_count = candidate_vote_count
             winner_name = candidate
         candidate_vote_count = 0
         data_rows = reset_csv(data_file)
 
-#Print results to terminal (Part III)
-print("****************************************")
-print(f"Winner: {winner_name}")
-print("****************************************")
+#Append election results to report for later output (Part III)
+report = report + f"\n************************* \nWinner: {winner_name}"
+f"\n*************************"
 
-#Write results to csv file in Analysis directory
-output_directory = os.path.join(curr_dir, "Analysis")
+#Print report to terminal and csv file
+print(report)
+output_directory = "Analysis"
 os.chdir(output_directory)
 with open('election_results.csv', 'w', newline='') as output_file:
-    output_file.write("Election results\r")
-    output_file.write("********************\r")
-    output_file.write(f"Total votes cast: {total_vote_count}\r")
-    output_file.write("********************\r")
-    output_file.write(winner_name)
+    output_file.write(report)
